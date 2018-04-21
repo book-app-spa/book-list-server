0bfcd5395cf503f072c329007ee0adc812f018dc
Mrebb <madhu.rebbana@gmail.com>
2018-04-21 10:45:16 -0700

    Added get requests for all books and detailed records

--- server.js
+++ server.js
@@ -16,6 +16,8 @@ client.connect();
 client.on('error', err => console.error(err));
 
 // Application Middleware
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
 app.use(cors());
 
 // API Endpoints
@@ -25,5 +27,30 @@ app.get('/api/v1/books', (req, res) => {
     .catch(console.error);
 });
 
+app.get('/api/v1/books/:id', (req,res) => {
+//    console.log("test");
+//    res.send("Hello");
+    client.query(`
+    select * from books where book_id = ${req.params.id};
+    `)
+    .then(results => res.send(results.rows))
+    .catch(console.error);
+});
+// app.put('api/v1/books/:id'), (req,res) => {
+//     console.log(req);
+//     client.query(`
+//     update books Set name = '${req.body.name}' where id = ${req.params.id};
+//     `)
+//     .then(results => res.send(results.rows))
+//     .catch(console.error);
+// }
+// app.delete('api/v1/books/:id'), (req,res) => {
+//     console.log(req);
+//     client.query(`
+//     delete from books where id = ${req.params.id}; 
+//     `)
+//     .then(results => res.sendStatus(200))
+//     .catch(console.error);//http DELETE :3000/api/v1/books/:id test
+// }
 //app.get('*', (req, res) => res.redirect(CLIENT_URL));
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
